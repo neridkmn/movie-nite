6b4039152ba485042b93a462a01c31f969b217c0
Neriman Dikmen <sanerneriman@gmail.com>
2024-05-30 16:33:17 -0400

    clean up group route

--- backend/src/routes/group.route.js
+++ backend/src/routes/group.route.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const nodemailer = require('nodemailer')
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 
-dotenv.config();
+// Load environment variables
+dotenv.config()
+
 const {
   createGroup,
   getGroupById,
@@ -19,22 +21,27 @@ const ERROR_REASON = require('../utils/errors')
 const authenticateToken = require('../middleware/jwt')
 const jwt = require('jsonwebtoken')
 
+// Create a new router
 const router = express.Router()
 
+// Generate JWT token
 const generateJwt = (data) => {
   const jwtSecretKey = process.env.JWT_SECRET_KEY
   const token = jwt.sign(data, jwtSecretKey)
   return token
 }
 
+// Define a route to test the group route
 router.get('/', (req, res) => {
   res.send('Hello from group route')
 })
 
+// Define a route to add a new group
 router.post('/new', authenticateToken, async (req, res) => {
   const { name, description, movie_date, venue } = req.body
   const { adminId, email } = req.admin
 
+  // Check if name, description, movie_date, venue is missing
   if (!name || !description || !movie_date || !venue) {
     return res.json({
       success: false,
@@ -46,6 +53,7 @@ router.post('/new', authenticateToken, async (req, res) => {
   }
 
   try {
+    // Create group
     const response = await createGroup({
       name,
       adminId,
@@ -54,6 +62,7 @@ router.post('/new', authenticateToken, async (req, res) => {
       venue,
     })
 
+    // Assign response id to groupId
     const groupId = response.id
 
     // Get user data for admin
@@ -74,8 +83,10 @@ router.post('/new', authenticateToken, async (req, res) => {
         adminId: req.admin.adminId,
       }
 
+      // Generate token
       const token = jwt.sign(data, jwtSecretKey)
 
+      // Set up nodemailer transporter
       const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -84,6 +95,7 @@ router.post('/new', authenticateToken, async (req, res) => {
         },
       })
 
+      // Generate token
       const userToken = generateJwt({
         email: adminAsUser.email,
         userId: adminAsUser.id,
@@ -91,6 +103,7 @@ router.post('/new', authenticateToken, async (req, res) => {
         purpose: 'poll',
       })
 
+      // Email options for movie suggestions
       const mailOptions = {
         from: process.env.MAIL_FROM,
         to: process.env.MAIL_TO, // SET TO adminAsUser.email
@@ -98,6 +111,7 @@ router.post('/new', authenticateToken, async (req, res) => {
         text: `Here is your MovieNite link: http://localhost:3000/preference?token=${userToken}`,
       }
 
+      // Send email with transporter
       transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
           console.log(error)
@@ -106,6 +120,7 @@ router.post('/new', authenticateToken, async (req, res) => {
         }
       })
 
+      // Send response to frontend
       res.json({
         success: true,
         group: response,
@@ -123,17 +138,21 @@ router.post('/new', authenticateToken, async (req, res) => {
   }
 })
 
+// Define a route to edit a group
 router.post('/edit/:id', authenticateToken, async (req, res) => {
   const { name, description, movie_date, venue } = req.body
   const groupId = req.params.id
 
   try {
+    // Get current group
     const currentGroup = await getGroupById(groupId)
 
+    // Check if group exists
     if (currentGroup.admin_id !== req.admin.adminId) {
       return res.status(401).send('You are not the owner of this group')
     }
 
+    // Update group
     const response = await updateGroup({
       groupId: currentGroup.id,
       name: name ? name : currentGroup.name,
@@ -151,8 +170,10 @@ router.post('/edit/:id', authenticateToken, async (req, res) => {
         adminId: req.admin.adminId,
       }
 
+      // Generate token
       const token = jwt.sign(data, jwtSecretKey)
 
+      // Send response to frontend
       res.json({
         success: true,
         group: response,
@@ -170,13 +191,17 @@ router.post('/edit/:id', authenticateToken, async (req, res) => {
   }
 })
 
+// Define a route to get a group by ID
 router.get('/:id', authenticateToken, async (req, res) => {
   const groupId = req.params.id
 
   try {
+    // Get group by ID
     const group = await getGroupById(groupId)
 
+    // Check if group exists
     if (!group) {
+      // Send error response to backend
       return res.status(403).json({
         success: false,
         error: {
@@ -186,13 +211,16 @@ router.get('/:id', authenticateToken, async (req, res) => {
       })
     }
 
+    // Check if admin is the owner of the group
     if (req.admin.adminId !== group.admin_id) {
       return res.status(401).send('You are not the owner of this group')
     }
 
     if (group) {
+      // Get users by group ID
       const users = await getUsersByGroupId(group.id)
 
+      // Refresh token
       const jwtSecretKey = process.env.JWT_SECRET_KEY
       let data = {
         email: req.admin.email,
@@ -200,8 +228,10 @@ router.get('/:id', authenticateToken, async (req, res) => {
         adminId: req.admin.adminId,
       }
 
+      // Generate token
       const token = jwt.sign(data, jwtSecretKey)
 
+      // Send response to frontend
       res.json({
         success: true,
         group: group,
@@ -220,24 +250,32 @@ router.get('/:id', authenticateToken, async (req, res) => {
   }
 })
 
+// Define a route to delete a group by ID
 router.post('/delete/:id', authenticateToken, async (req, res) => {
   const groupId = req.params.id
 
   try {
+    // Get group by ID
     const group = await getGroupById(groupId)
 
     if (!group) {
+      // Send error response to backend
       return res.status(404).send('Group not found')
     }
 
+    // Check if admin is the owner of the group
     if (req.admin.adminId !== group.admin_id) {
       return res.status(401).send('You are not the owner of this group')
     }
 
+    // Delete group
     const response = await deleteGroupById(groupId)
+    // Delete membership by group ID
     await deleteMembershipByGroupId(groupId)
 
+    // Check if group was deleted
     if (response) {
+      // Refresh token
       const jwtSecretKey = process.env.JWT_SECRET_KEY
       let data = {
         email: req.admin.email,
@@ -245,8 +283,10 @@ router.post('/delete/:id', authenticateToken, async (req, res) => {
         adminId: req.admin.adminId,
       }
 
+      // Generate token
       const token = jwt.sign(data, jwtSecretKey)
 
+      // Send response to frontend
       res.json({
         success: true,
         token: token,
