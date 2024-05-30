fb2a159868d7d84c3de6ca412f286a69154cd7b2
Neriman Dikmen <sanerneriman@gmail.com>
2024-05-30 16:56:45 -0400

    Add comments to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+// Initialize express and set up routes
+const express = require('express')
+// Import body-parser to parse request body
+const bodyParser = require('body-parser')
 
 // Initialize express
-const app = express();
+const app = express()
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Parse application/json to use req.body
+app.use(bodyParser.json())
 
 // Routes
-app.use('/api/admins', require('./routes/admins.route.js'));
-app.use('/api/group', require('./routes/group.route.js'));
-app.use('/api/movie', require('./routes/movie.route.js'));
-app.use('/api/membership', require('./routes/membership.route.js'));
+app.use('/api/admins', require('./routes/admins.route.js'))
+app.use('/api/group', require('./routes/group.route.js'))
+app.use('/api/movie', require('./routes/movie.route.js'))
+app.use('/api/membership', require('./routes/membership.route.js'))
 
-module.exports = app;
+module.exports = app
