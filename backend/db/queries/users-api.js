847ef9cdce90d1eedf923480d748b28791940cd4
Neriman Dikmen <sanerneriman@gmail.com>
2024-05-30 16:49:10 -0400

    format queries

--- backend/db/queries/users-api.js
+++ backend/db/queries/users-api.js
@@ -1,79 +1,85 @@
-const db = require("../connection");
+const db = require('../connection')
 
 const getUserByEmail = (email) => {
   return db
-    .query(`SELECT * FROM users WHERE email = $1;`,
-            [email]
-            )
-    .then(data => data.rows[0]);
-};
+    .query(`SELECT * FROM users WHERE email = $1;`, [email])
+    .then((data) => data.rows[0])
+}
 
 const createUser = ({ name, email, adminId }) => {
   return db
-    .query(`INSERT INTO users (name, email, admin_id, created_at, updated_at)
+    .query(
+      `INSERT INTO users (name, email, admin_id, created_at, updated_at)
             VALUES ($1, $2, $3, NOW(), NOW()) 
             RETURNING *;`,
-            [name, email, adminId]
+      [name, email, adminId]
     )
-    .then(data => data.rows[0])
+    .then((data) => data.rows[0])
 }
 
 const addMembership = ({ userId, groupId }) => {
   return db
-    .query(`INSERT INTO membership (user_id, group_id, created_at)
+    .query(
+      `INSERT INTO membership (user_id, group_id, created_at)
             VALUES ($1, $2, NOW()) 
             RETURNING *;`,
-            [userId, groupId]
+      [userId, groupId]
     )
-    .then(data => data.rows[0])  
-};
+    .then((data) => data.rows[0])
+}
 
 const getMembershipByUserAndGroupId = ({ userId, groupId }) => {
   return db
-    .query(`SELECT * FROM membership WHERE user_id = $1 AND group_id = $2`, [userId, groupId])
-    .then(data => data.rows[0]);
-};
+    .query(`SELECT * FROM membership WHERE user_id = $1 AND group_id = $2`, [
+      userId,
+      groupId,
+    ])
+    .then((data) => data.rows[0])
+}
 
 const deleteMembership = (id) => {
   return db
     .query(`DELETE FROM membership WHERE id = $1 RETURNING *;`, [id])
-    .then(data => data.rows[0])
-};
+    .then((data) => data.rows[0])
+}
 
 const getUserById = (userId) => {
   return db
     .query(`SELECT * FROM users WHERE id = $1`, [userId])
-    .then(data => data.rows[0])
+    .then((data) => data.rows[0])
 }
 
 const getUsersByAdminId = (adminId) => {
   return db
     .query(`SELECT * FROM users WHERE admin_id = $1;`, [adminId])
-    .then(data => {
+    .then((data) => {
       if (!data) {
-        return null;
+        return null
       }
-      return data.rows;
-    });
-};
+      return data.rows
+    })
+}
 
 const getUsersByGroupId = (groupId) => {
   return db
-  .query(`SELECT * FROM membership 
+    .query(
+      `SELECT * FROM membership 
   JOIN users ON membership.user_id = users.id
-  WHERE group_id = $1;`, [groupId]) 
-  .then(data => {
-    if (!data) {
-      return null;
-    }
-    return data.rows;
-  });
+  WHERE group_id = $1;`,
+      [groupId]
+    )
+    .then((data) => {
+      if (!data) {
+        return null
+      }
+      return data.rows
+    })
 }
 
 const deleteMembershipByGroupId = (groupId) => {
   return db
     .query(`DELETE FROM membership WHERE group_id = $1 RETURNING *;`, [groupId])
-    .then(data => data.rows[0])
+    .then((data) => data.rows[0])
 }
 
 module.exports = {
@@ -86,4 +92,4 @@ module.exports = {
   getUsersByAdminId,
   getUsersByGroupId,
   deleteMembershipByGroupId,
-};
+}
