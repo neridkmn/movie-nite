9e4d987fa26152df9afed3556357cdb8e7892376
Neriman Dikmen <sanerneriman@gmail.com>
2024-05-30 16:58:53 -0400

    Add comments to index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,10 @@
-require("dotenv").config();
+//returns an object representing the parsed .env file
+require('dotenv').config()
 
-const app = require ('./app');
-const PORT = 8001;
+//take the app from app.js
+const app = require('./app')
+const PORT = 8001
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
-
-
-
-
+  console.log(`Example app listening on port ${PORT}`)
+})
