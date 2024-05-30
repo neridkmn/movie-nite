9380e500d0e484ac422fbc31a0970561801afeee
Neriman Dikmen <sanerneriman@gmail.com>
2024-05-30 16:55:02 -0400

    Format utils

--- backend/src/utils/getMovies.js
+++ backend/src/utils/getMovies.js
@@ -1,14 +1,24 @@
+// Get the movies from the result object
 const getMovies = (result) => {
-  const result_keys = Object.keys(result);
-  if (result_keys.includes('suggestions') && Array.isArray(result.suggestions)) {
-    return result.suggestions;
+  // Check if the result object has a suggestions key
+  const result_keys = Object.keys(result)
+
+  // If the result object has a suggestions key and it is an array, return the suggestions array
+  if (
+    result_keys.includes('suggestions') &&
+    Array.isArray(result.suggestions)
+  ) {
+    return result.suggestions
+  }
+  // If the result object has a suggested_movies key and it is an array, return the suggested_movies array
+  if (
+    result_keys.includes('suggested_movies') &&
+    Array.isArray(result.suggested_movies)
+  ) {
+    return result.suggested_movies
   }
 
-  if (result_keys.includes('suggested_movies') && Array.isArray(result.suggested_movies)) {
-    return result.suggested_movies;
-  }
+  return []
+}
 
-  return [];
-};
-
-module.exports = getMovies;
+module.exports = getMovies
