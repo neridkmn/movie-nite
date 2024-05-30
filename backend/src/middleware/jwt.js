f885457868894f1dea8523f52b889688691aa27c
Neriman Dikmen <sanerneriman@gmail.com>
2024-05-30 16:47:56 -0400

    add comments to admins route and middleware

--- backend/src/middleware/jwt.js
+++ backend/src/middleware/jwt.js
@@ -1,20 +1,27 @@
-const dotenv = require('dotenv');
-const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv')
+const jwt = require('jsonwebtoken')
 
-dotenv.config();
+// Load environment variables
+dotenv.config()
 
 function authenticateToken(req, res, next) {
-  const token = req.headers['authorization'];
+  // Get token from header
+  const token = req.headers['authorization']
 
-  if (token === null) return res.sendStatus(401);
+  //Check if token is null
+  if (token === null) return res.sendStatus(401)
+
+  //Verify token using jwt verify method
+  //If token is valid, set admin to req.admin and call next
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, admin) => {
+    //If token is invalid return 403 status
+    if (err) return res.sendStatus(403)
 
-    if (err) return res.sendStatus(403);
-
+    //Set admin to req.admin
     req.admin = admin
 
     next()
   })
 }
 
-module.exports = authenticateToken;
+module.exports = authenticateToken
